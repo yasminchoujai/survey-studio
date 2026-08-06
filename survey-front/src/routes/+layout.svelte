83640721fc5e536ca6a792d02af59f6097659b3d
yasmin choujai <jasminchoujai@gmail.com>
2026-08-06 13:14:41 +0100

    Refine layout and remove unused preview components

--- survey-front/src/routes/+layout.svelte
+++ survey-front/src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script>
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import surveyStudioLogo from '$lib/assets/survey.logo.png';
 	import ToastContainer from '$lib/components/ui/ToastContainer.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={surveyStudioLogo} />
 </svelte:head>
 
 {@render children()}
